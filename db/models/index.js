28f9a61e1d5c8bd56441406cb55c178e66d657d1
Peter Tan <pxhtan@gmail.com>
2017-07-14 16:52:36 -0700

    Add schema users / groups / channels / users_groups / messages and their models

--- db/models/index.js
+++ db/models/index.js
@@ -1,2 +1,5 @@
 module.exports.Auth = require('./auths');
 module.exports.Profile = require('./profiles');
+module.exports.User = require('./users');
+module.exports.Group = require('./groups');
+module.exports.Channel = require('./channels');
