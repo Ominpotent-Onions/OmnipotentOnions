074a64063e271f811d86a473a803646dec99badf
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-15 16:42:24 -0700

    implement Redux front-end with dummy data, incomplete message input

--- client/src/app.js
+++ client/src/app.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import reducers from './reducers';
 
+// import Login from './components/Login/index.js';
 import Main from './components/index';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
