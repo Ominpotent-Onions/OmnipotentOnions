97b48a2aa830de45f481a03d07279da9b1a610fb
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-15 16:42:39 -0700

    implement Redux front-end with dummy data, incomplete message input

--- client/src/components/messages_board.js
+++ client/src/components/messages_board.js
@@ -15,7 +15,6 @@ class MessageBoard extends Component {
 
     const messages = _.map(this.props.messages, message => {
       return (
-<<<<<<< 632e1b211700ed4331730cc0d174a413085e50c7
         <Segment key={message.id}> 
           <h4>{message.user}</h4>
           <div>{message.text}</div>
@@ -23,15 +22,6 @@ class MessageBoard extends Component {
       );
     });
 
-=======
-        <div key={message.id}> 
-          <h4> {message.user} </h4>
-          <div> {message.text} </div>
-        </div>
-      )
-    })
-    
->>>>>>> implement Redux front-end with dummy data, incomplete message input
     return (
       <div>
         {messages}
