4d19e088994d350a5bec283c81c9b8e1c7595271
Peter Tan <pxhtan@gmail.com>
2017-07-15 23:45:11 -0700

    Implement hasOne, create func

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,34 @@
 consts = require('../../db/models');
 
-module.exports.getAll = (req, res) => {
+module.exports.create = (req, res) => {
+  models.User.forge({ name: req.body.name, 
+    token: req.body.token, 
+    salt: req.body.salt })
+    .save()
+    .then(result => {
+      res.status(201).send(result.omit('token', 'salt'));
+    })
+    .catch(err => {
+      console.log('server/controllers/user.jsER CONSTRAINT: ', err.constraint);
+      if (err.constraint) {
+        return res.stats(403);
+      }
+      res.status(500).send(err);
+    });
+};
 
+module.exports.getOne = (req, res) => {
+  models.User.where({ id: req.params.id }).fetch()
+    .then(profile => {
+      if (!profile) {
+        throw profile;
+      }
+      res.status(200).send(profile);
+    }) 
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
 };
