fdba94b05cadc5ac3f827873dffc65110e5ac50c
Peter Tan <pxhtan@gmail.com>
2017-07-17 09:51:02 -0700

    fix rebase issues

--- db/models/index.js
+++ db/models/index.js
@@ -1,5 +1,6 @@
 module.exports.Auth = require('./auths');
 module.exports.Profile = require('./profiles');
+// add other models to be exported
 module.exports.User = require('./users');
 module.exports.Group = require('./groups');
 module.exports.Channel = require('./channels');
