bebd0941c41167ab3ee5ce64867b3cbf4705f376
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-17 19:58:26 -0700

    wire up reducer to socket

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -64,7 +64,7 @@ export let fetchChannels = function(group) {
 export let fetchMessages = function(channel) {
   // replace with real ajax request
   // const request = axios.get(``);
-  
+
   // dummy request
   const request = [
     {
