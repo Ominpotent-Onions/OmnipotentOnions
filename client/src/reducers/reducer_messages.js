0c0e61e91fb613e1ffae68545403edacc99f307d
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-17 22:05:16 -0700

    front-end implementation of socket created

--- client/src/reducers/reducer_messages.js
+++ client/src/reducers/reducer_messages.js
@@ -9,10 +9,6 @@ export default function(state = {}, action) {
     return newObj;
   case FETCH_MESSAGES:
     return _.mapKeys(action.payload, 'id');
-  case CREATE_MESSAGE:
-    let newState = state;
-    newState[action.payload.id] = action.payload;
-    return newState;
   default:
     return state;
   }   
