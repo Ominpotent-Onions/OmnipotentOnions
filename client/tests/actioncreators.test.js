032e5e371ddff7ba56d4a2bea2d4ded87e0e0d7f
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-19 19:21:21 -0700

    remove component tests and add synchronous createMessage test

---
+++ client/tests/actioncreators.test.js
@@ -0,0 +1,20 @@
+import * as actions from '../src/actions/index.js';
+
+describe('actions', () => {
+  it('should fetch your profile', () => {
+    const profile = {
+      id: 2,
+      first: 'Shi Hao',
+      last: 'Hong',
+      display: 'Shi Hao Hong',
+      email: 'shihaohong94@gmail.com'
+    }
+
+    const expectedAction = {
+      type: actions.CREATE_MESSAGE,
+      payload: profile
+    };
+
+    expect(actions.createMessage(profile)).toEqual(expectedAction);
+  });
+})
--- client/tests/profile.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Profile from '../src/components/profile';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<Profile />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
