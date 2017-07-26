fbde437eba86492c47f8d099f1ecb43991bad2e9
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-07-26 16:58:53 -0700

    add profile page

--- client/src/reducers/reducer_profile.js
+++ client/src/reducers/reducer_profile.js
@@ -1,21 +1,14 @@
 import { FETCH_PROFILE, UPDATE_PROFILE_BIO, UPDATE_NICKNAME } from '../actions';
 
 export default function(state = {}, action) {
-  console.log('action', action.payload);
-  console.log('state', state);
   switch (action.type) {
   case FETCH_PROFILE:
-    // console.log('profile fetched');
     return action.payload;
   case UPDATE_PROFILE_BIO:
     return action.payload.data;
   case UPDATE_NICKNAME:
     return action.payload.data;
   default:
-    // console.log('action default', action);
-    // console.log('hit default!! state ', state);
     return state;
-  
   }
-
 }
