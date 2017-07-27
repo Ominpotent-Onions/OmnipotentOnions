6ed551256d06e7e19ffb1ce4f9b968d8fab1ce58
Peter Tan <pxhtan@gmail.com>
2017-07-27 12:09:58 -0700

    Create models for events and attendees, controller for events

---
+++ server/controllers/attendees.js
@@ -0,0 +1,9 @@
+const models = require('../../db/models');
+
+module.exports.join = (req, res) => {
+  models.Attendee;
+};
+
+module.exports.unjoin = (req, res) => {
+
+};
---
+++ server/controllers/events.js
@@ -0,0 +1,50 @@
+const models = require('../../db/models');
+
+module.exports.createEvent = (req, res) => {
+  models.Event.forge({
+    date: req.body.date,
+    address: req.body.address,
+    group_id: req.params.groupId,
+    creator: req.bodhy.profileId,
+    time: req.body.time
+  })
+  .save()
+  .then(event => {
+    modles.Attendee.forge({
+      event_id: event.id,
+      profile_id: event.creator
+    })
+    .save()
+    .then(attendee => {
+      models.Event.where({ group_id: req.params.groupId }).fetchAll()
+      .then(events => {
+        res.status(201).send(events);
+      })
+      .error(err => {
+        res.status(500).send(err);
+      });
+    });
+  })
+  .error(err => {
+    res.status(500).send(err);
+  });
+};
+
+module.exports.deleteEvent = (req, res) => {
+  models.Event.where({ id: req.body.eventId }).fetch()
+    .then(event => {
+      if (!event) {
+        throw event;
+      }
+      return event.destroy();
+    })
+    .then(() => {
+      res.status(500).send('Event has been deleted');
+    })
+    .error(err => {
+      res.status(503).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+};
