99fb033a46668ed09b1429e65ccd79eaae832302
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-07-28 16:17:08 -0700

    rebasing

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -181,6 +181,7 @@ export let deleteEvent = function(eventId, groupId) {
 };
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 // export let fetchEvents = function(groupId) {
@@ -192,6 +193,8 @@ export let deleteEvent = function(eventId, groupId) {
 // };
 =======
 >>>>>>> render group list
+=======
+>>>>>>> rebasing
 export let fetchEvents = function(groupId) {
   console.log('fetch events in action ', groupId);
   let events = axios.get(`/events/${groupId}`);
@@ -201,9 +204,12 @@ export let fetchEvents = function(groupId) {
   };
 };
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> render group list
 >>>>>>> render group list
+=======
+>>>>>>> rebasing
 
 export let fetchEvent = function(groupId, eventId) {
   let event = axios.get(`/events/${groupId}/${eventId}`);
