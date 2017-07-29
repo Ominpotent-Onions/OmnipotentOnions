c9e6da040ccfc702be6d9aa0b4b41bfe8f1b48a8
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-07-28 19:09:57 -0700

    Fix message function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -164,21 +164,6 @@ export let deleteEvent = function(eventId, groupId) {
   };
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-// export let fetchEvents = function(groupId) {
-//   let events = axios.get(`/events/${groupId}`);
-//   return {
-//     type: FETCH_EVENTS,
-//     payload: events
-//   };
-// };
-=======
->>>>>>> render group list
-=======
->>>>>>> rebasing
 export let fetchEvents = function(groupId) {
   console.log('fetch events in action ', groupId);
   let events = axios.get(`/events/${groupId}`);
@@ -187,13 +172,6 @@ export let fetchEvents = function(groupId) {
     payload: events
   };
 };
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> render group list
->>>>>>> render group list
-=======
->>>>>>> rebasing
 
 export let fetchEvent = function(groupId, eventId) {
   let event = axios.get(`/events/${groupId}/${eventId}`);
