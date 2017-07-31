24aac25ffa1ae7d80ccd226ca8e2fcedb67419f1
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-07-31 11:44:03 -0700

    add test for server side groups

---
+++ server/tests/events.routes.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const request = require('supertest');
+const express = require('express');
+const expect = require('chai').expect;
+const app = require('../app.js');
+const dbUtils = require('../../db/lib/utils.js');
+
+describe('Profile API', function() {
+  beforeEach(function(done) {
+    dbUtils.rollbackMigrate(done);
+  });
+
+  afterEach(function(done) {
+    dbUtils.rollback(done);
+  });
+  
+  it('accepts POST request to /events/:groupId', function(done) {
+    var event = {
+      eventName: 'Test Party',
+      location: '944 Market St. San Francisco',
+      creator: 1,
+      startTime: '16:00',
+      startDate: '2017-07-30',
+      endTime: '22:00',
+      endDate: '2017-07-30',
+      detail: 'work pls'
+    };
+		
+    request(app)
+      .post('/groups/createGroup/genericName')
+      .query({
+        id: 1,
+        shortID: '123JAS'
+      });
+    request(app)
+			.post('/events/1')
+			.send(event)
+			.expect(res => {
+  console.log('THIS IS WHAT I GOT FROM MY EVENTS ', res.body);
+})
+			.expect(201, event)
+      .end(done);
+  });
+			
+  it('accepts GET request to /events/:groupId', function(done) {
+    request(app)
+      .get('/events/1')
+      .expect(200)
+      .end(done);
+  });
+});
+
